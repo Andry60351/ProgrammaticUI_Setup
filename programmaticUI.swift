1445e44bc535a944ce844fb6c573e4b91872983a
Andry60351 <49724349+Andry60351@users.noreply.github.com>
2020-06-01 11:56:58 -0700

    Update programmaticUI.swift

--- programmaticUI.swift
+++ programmaticUI.swift
@@ -1,11 +1,11 @@
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        let firstScreen = putFirstScreenHere()
-        let navigationController = UINavigationController(rootViewController: firstScreen)
+    let firstScreen = putFirstScreenHere()
+    let navigationController = UINavigationController(rootViewController: firstScreen)
 
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
-    }
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+    window?.windowScene = windowScene
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
+}
